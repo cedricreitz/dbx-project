4b26044538f790b5cec03eae246e8f521fff0267
Cedric Reitz <cedric.reitz@redcare-pharmacy.com>
2025-07-21 19:03:17 +0200

    added logging

---
+++ src/macros/logging.sql
@@ -0,0 +1,22 @@
+{% macro logging() %}
+    {# Only run at execution time #}
+    {% if execute %}
+        {# Pull out each value #}
+        {{ log(
+            "Target database: " ~ this.database,
+            info = true
+        ) }}
+        {{ log(
+            "Target schema: " ~ this.schema,
+            info = true
+        ) }}
+        {{ log(
+            "Target table: " ~ this.identifier,
+            info = true
+        ) }}
+        {{ log(
+            "full_refresh is " ~ (config.get('full_refresh', False) | string),
+            info = true
+        ) }}
+    {% endif %}
+{% endmacro %}
