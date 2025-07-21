d19eb15795a5ff4cbbaa43693f6a40efbef0b891
Cedric Reitz <cedric.reitz@redcare-pharmacy.com>
2025-07-21 20:53:24 +0200

    improvements

--- src/macros/auto_resolve_location.sql
+++ src/macros/auto_resolve_location.sql
@@ -1,71 +1,61 @@
 {# Override dbt default database macro #}
-{% macro generate_database_name(
-    custom_database_name,
-    node
-  ) -%}
-  {%- if custom_database_name -%}
-    {{ custom_database_name | trim }}
-  {%- else -%}
-    {% set folder_source = node.path.split("/") [0] %}
-    {% set filename_source = node.name.split(".") [0] %}
-    {% set env = var(
-      'env',
-      target.name
-    ) %}
-    {# Check if folder and filename match #}
-    {%- if folder_source == filename_source -%}
-      {# Check if folder exists in database layers #}
-      {% if not var(
-          'layer_order',
-          none
-        ) %}
-        {% do exceptions.raise_compiler_error("`layer_order` variable is not set in dbt_project.yml. Please configure layer_order.") %}
-      {% endif %}
-
-      {{ env ~ '_' ~ filename_source }}
+{% macro generate_database_name(custom_database_name, node) -%}
+    {%- if custom_database_name -%} {{ custom_database_name | trim }}
     {%- else -%}
-      {{ exceptions.raise_compiler_error(
-        "Filename and folder location do not match. \n Please make sure you name the file/folder according to its location following the database/schema and <datbase>.<schema>.<tablename>.sql format. Affected model: " ~ node.path
-      ) }}
+        {% set folder_source = node.path.split("/")[0] %}
+        {% set filename_source = node.name.split(".")[0] %}
+        {% set env = var("env", target.name) %}
+        {# Check if folder and filename match #}
+        {%- if folder_source == filename_source -%}
+            {# Check if folder exists in database layers #}
+            {% if not var("layer_order", none) %}
+                {% do exceptions.raise_compiler_error(
+                    "`layer_order` variable is not set in dbt_project.yml. Please configure layer_order."
+                ) %}
+            {% endif %}
+
+            {{ env ~ "_" ~ filename_source }}
+        {%- else -%}
+            {{
+                exceptions.raise_compiler_error(
+                    "Filename and folder location do not match. \n Please make sure you name the file/folder according to its location following the database/schema and <datbase>.<schema>.<tablename>.sql format. Affected model: "
+                    ~ node.path
+                )
+            }}
+        {%- endif -%}
     {%- endif -%}
-  {%- endif -%}
 {%- endmacro %}
 
+
 {# Override dbt default schema macro #}
-{% macro generate_schema_name(
-    custom_schema_name,
-    node
-  ) -%}
-  {%- if custom_schema_name -%}
-    {{ custom_schema_name | trim }}
-  {%- else -%}
-    {% set folder_source = node.path.split("/") [1] %}
-    {% set filename_source = node.name.split(".") [1] %}
-    {% set env = var(
-      'env',
-      target.name
-    ) %}
-    {%- if folder_source == filename_source -%}
-      {{ filename_source }}
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- if custom_schema_name -%} {{ custom_schema_name | trim }}
     {%- else -%}
-      {{ exceptions.raise_compiler_error(
-        "Filename and folder location do not match. \n Please make sure you name the file/folder according to its location following the database/schema and <datbase>.<schema>.<tablename>.sql format. Affected model: " ~ node.path
-      ) }}
+        {% set folder_source = node.path.split("/")[1] %}
+        {% set filename_source = node.name.split(".")[1] %}
+        {% set env = var("env", target.name) %}
+        {%- if folder_source == filename_source -%} {{ filename_source }}
+        {%- else -%}
+            {{
+                exceptions.raise_compiler_error(
+                    "Filename and folder location do not match. \n Please make sure you name the file/folder according to its location following the database/schema and <datbase>.<schema>.<tablename>.sql format. Affected model: "
+                    ~ node.path
+                )
+            }}
+        {%- endif -%}
     {%- endif -%}
-  {%- endif -%}
 {%- endmacro %}
 
 {# Override dbt default alias macro #}
-{% macro generate_alias_name(
-    custom_alias_name,
-    node
-  ) -%}
-  {%- if custom_alias_name -%}
-    {{ custom_alias_name | trim }}
+{% macro generate_alias_name(custom_alias_name, node) -%}
+    {%- if custom_alias_name -%} {{ custom_alias_name | trim }}
 
     {%- elif node.version -%}
-    {{ return(node.name.split(".") [2] ~ "_v" ~ (node.version | replace(".", "_"))) }}
-  {%- else -%}
-    {{ node.name.split(".") [2] }}
-  {%- endif -%}
+        {{
+            return(
+                node.name.split(".")[2] ~ "_v" ~ (node.version | replace(".", "_"))
+            )
+        }}
+    {%- else -%} {{ node.name.split(".")[2] }}
+    {%- endif -%}
 {%- endmacro %}
--- src/macros/logging.sql
+++ src/macros/logging.sql
@@ -2,21 +2,14 @@
     {# Only run at execution time #}
     {% if execute %}
         {# Pull out each value #}
-        {{ log(
-            "Target database: " ~ this.database,
-            info = true
-        ) }}
-        {{ log(
-            "Target schema: " ~ this.schema,
-            info = true
-        ) }}
-        {{ log(
-            "Target table: " ~ this.identifier,
-            info = true
-        ) }}
-        {{ log(
-            "full_refresh is " ~ (config.get('full_refresh', False) | string),
-            info = true
-        ) }}
+        {{ log("Target database: " ~ this.database, info=true) }}
+        {{ log("Target schema: " ~ this.schema, info=true) }}
+        {{ log("Target table: " ~ this.identifier, info=true) }}
+        {{
+            log(
+                "full_refresh is " ~ (config.get("full_refresh", False) | string),
+                info=true,
+            )
+        }}
     {% endif %}
 {% endmacro %}
